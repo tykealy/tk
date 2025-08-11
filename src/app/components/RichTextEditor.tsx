6f81115770f0053b48f87dc54af0dc72f9887770
tykealy <tykeaboyloy@gmail.com>
2025-08-12 00:08:21 +0700

    added tiptap

---
+++ src/app/components/RichTextEditor.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
+
+const RichTextEditor = () => {
+  return (
+    <div className="w-full">
+      <SimpleEditor />
+    </div>
+  )
+}
+
+export default RichTextEditor
